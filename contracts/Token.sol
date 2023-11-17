e89c942bf7fda3fb4c3ed0c72cd050b557a8bf76
Brooke Smith <brooke@oehmsmith.com>
2023-11-17 14:08:22 +1100

    Stage 2

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.4;
 
 contract Token {
     uint public totalSupply;
+    string public name="alpha";
+    string public symbol="abc";
+    uint8 public decimals=18;
 
     constructor(){
 
