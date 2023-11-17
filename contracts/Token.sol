fa8f05ef7ff299e936bdc630bf41e4a5ea4038d0
Brooke Smith <brooke@oehmsmith.com>
2023-11-17 14:11:15 +1100

    Stage 3

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,7 +7,13 @@ contract Token {
     string public symbol="abc";
     uint8 public decimals=18;
 
+    mapping (address => uint256) balance;
+
     constructor(){
 
+    }
+
+    function balanceOf(address _a) external view returns (uint256) {
+        return balance[_a];
     }
 }
