64b759e2859ecf776191b274e8930167f350fd63
Brooke Smith <brooke@oehmsmith.com>
2023-11-17 14:14:36 +1100

    Stage 4 - Minting

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.4;
 
 contract Token {
     uint public totalSupply;
-    string public name="alpha";
-    string public symbol="abc";
-    uint8 public decimals=18;
+    string public name = "alpha";
+    string public symbol = "abc";
+    uint8 public decimals = 18;
 
-    mapping (address => uint256) balance;
+    mapping(address => uint256) balance;
 
     constructor(){
-
+        totalSupply = 1000 * 10 ** 18;
+        balance[msg.sender] = totalSupply;
     }
 
     function balanceOf(address _a) external view returns (uint256) {
